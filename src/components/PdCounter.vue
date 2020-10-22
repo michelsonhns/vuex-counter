8f3f348291d6b4f64f2a516d4be6bf859d39254a
mich <michelson@poucedigital.com>
2020-10-22 20:34:38 +0200

    Create PdCounter.vue

---
+++ src/components/PdCounter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>{{ currentValue }}</h2>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      increment() {
+        this.$store.commit('INCREMENT_COUNTER');// on utilise la variable $store pour la mutation, commiter une action pour modifier le state.
+      },
+      decrement() {
+        this.$store.commit('DECREMENT_COUNTER');
+      }
+    },
+    computed: {
+      currentValue() {
+        return this.$store.state.currentValue;
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
