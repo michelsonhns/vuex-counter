17e9ff6ca625f8741fca52dbacfedee24c65ce14
mich <michelson@poucedigital.com>
2020-10-22 20:34:32 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PdCounter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PdCounter from "@/components/PdCounter"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    PdCounter,
+  },
 }
 </script>
 
